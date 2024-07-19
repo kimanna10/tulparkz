c034453501281f8106685d60d388eea48fbda567
Anna <kimannais1711@gmail.com>
2024-07-19 15:00:40 +0500

    Админ панель

--- admin/js/drop-modal.js
+++ admin/js/drop-modal.js
@@ -1,18 +1,52 @@
 $(document).ready(function() {
-    $('.meeting').on('click', function() {
-        var $parent = $(this).closest('.table-block__status-item');
-        
-        // Закрываем все другие открытые дропдауны
-        $('.table-block__status-item').not($parent).removeClass('active');
+    let $currentStatusItem = null;
 
-        // Переходим к дропдауну текущего элемента
-        $parent.toggleClass('active');
+    // Открытие дропа
+    function openDropdown($statusItem) {
+        const offset = $statusItem.offset();
+        $('#dropdown-content').css({
+            top: offset.top + $statusItem.outerHeight(),
+            left: offset.left,
+            display: 'block'
+        }).show();
+        $currentStatusItem = $statusItem;
+
+        // Установка состояния выбранного пункта
+        const currentText = $statusItem.find('.meeting').text();
+        $('#dropdown-content input[name="radios"]').each(function() {
+            if ($(this).siblings('label').text() === currentText) {
+                $(this).prop('checked', true);
+            } else {
+                $(this).prop('checked', false);
+            }
+        });
+    }
+
+    // Закрытие дропа
+    function closeDropdown() {
+        $('#dropdown-content').hide();
+    }
+
+    // Открытие дропа при клике на элемент статуса
+    $('.table-block__status-item').on('click', function(event) {
+        event.stopPropagation(); // Предотвращаем всплытие события
+        openDropdown($(this));
     });
 
-    // Закрытие дропдауна при клике вне его области
+    // Обработка выбора и сохранения
+    $('.save-button').on('click', function(event) {
+        event.stopPropagation(); // Предотвращаем всплытие события
+        const selectedText = $('#dropdown-content input[name="radios"]:checked').siblings('label').text();
+        if ($currentStatusItem) {
+            $currentStatusItem.find('.meeting').text(selectedText);
+        }
+        closeDropdown();
+    });
+
+    // Закрытие дропа при клике вне его области
     $(document).on('click', function(event) {
-        if (!$(event.target).closest('.table-block__status-item').length) {
-            $('.table-block__status-item').removeClass('active');
+        if (!$(event.target).closest('.table-block__status-item, #dropdown-content').length) {
+            closeDropdown();
         }
     });
 });
